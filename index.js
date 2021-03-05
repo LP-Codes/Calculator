4eda8d07d26f1833dba22c3c1bc74e333d75e76d
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-05 13:54:42 +0530

    test

--- index.js
+++ index.js
@@ -6,75 +6,52 @@ function clearall() {
 }
 
 function divd1() {
-    let lastChar = document.getElementById("inpt1").value;
-    // console.log(lastChar.slice(-1))
-     
-    
-    if (lastChar.slice(-1)=="/") {
-    console.log("repeated press ignoring")
-    
-    }
-    else{
-        document.getElementById("inpt1").value += "/";
-    }
-  
-//   document.getElementById("inpt1").value += "/";
+  let lastChar = document.getElementById("inpt1").value;
+  // console.log(lastChar.slice(-1))
+
+  if (lastChar.slice(-1) == "/") {
+    console.log("repeated press ignoring");
+  } else {
+    document.getElementById("inpt1").value += "/";
+  }
+
+  //   document.getElementById("inpt1").value += "/";
 }
 function plus(e) {
-   let lastChar = document.getElementById("inpt1").value;
-    // console.log(lastChar.slice(-1))
-     
-    
-    if (lastChar.slice(-1)=="+") {
-    console.log("repeated press ignoring")
-    
-    }
-    else{
-        document.getElementById("inpt1").value += "+";
-    }
-  
-    
- 
+  let lastChar = document.getElementById("inpt1").value;
+  // console.log(lastChar.slice(-1))
 
-
-  
-
-  
-      
-    
-  
+  if (lastChar.slice(-1) == "+") {
+    console.log("repeated press ignoring");
+  } else {
+    document.getElementById("inpt1").value += "+";
+  }
 }
 function minus() {
-    let lastChar = document.getElementById("inpt1").value;
-    // console.log(lastChar.slice(-1))
-     
-    
-    if (lastChar.slice(-1)=="-") {
-    console.log("repeated press ignoring")
-    
-    }
-    else{
-        document.getElementById("inpt1").value += "-";
-    }
-  
+  let lastChar = document.getElementById("inpt1").value;
+  // console.log(lastChar.slice(-1))
+
+  if (lastChar.slice(-1) == "-") {
+    console.log("repeated press ignoring");
+  } else {
+    document.getElementById("inpt1").value += "-";
+  }
 }
 function into() {
-    let lastChar = document.getElementById("inpt1").value;
-    // console.log(lastChar.slice(-1))
-     
-    console.log(lastChar.slice(1))
-    if (lastChar.slice(-1)=="*") {
+  let lastChar = document.getElementById("inpt1").value;
+  // console.log(lastChar.slice(-1))
+
+  console.log(lastChar.slice(1));
+  if (lastChar.slice(-1) == "*") {
     console.log(lastChar.slice(-1));
-    console.log("repeated press ignoring")
-    
-    }
-    else{
-        document.getElementById("inpt1").value += "*";
-    }
-  
+    console.log("repeated press ignoring");
+  } else {
+    document.getElementById("inpt1").value += "*";
+  }
 }
 
 function inpt1() {
+  console.log('a')
   document.getElementById("inpt1").value += 1;
 }
 function inpt2() {
@@ -130,11 +107,16 @@ function equal1() {
   }
 }
 
-document.getElementById("btn1").addEventListener("click", inpt1);
-document.getElementById("btn2").addEventListener("click", inpt2);
 document.getElementById("clear").addEventListener("click", clearall);
 document.getElementById("divd").addEventListener("click", divd1);
 document.getElementById("eqal2").addEventListener("click", equal1);
+document.getElementById("dot").addEventListener("click", inptdot);
+document.getElementById("plus").addEventListener("click", plus);
+document.getElementById("into").addEventListener("click", into);
+document.getElementById("minus").addEventListener("click", minus);
+
+document.getElementById("btn1").addEventListener("click", inpt1);
+document.getElementById("btn2").addEventListener("click", inpt2);
 document.getElementById("btn3").addEventListener("click", inpt3);
 document.getElementById("btn4").addEventListener("click", inpt4);
 document.getElementById("btn5").addEventListener("click", inpt5);
@@ -143,8 +125,3 @@ document.getElementById("btn7").addEventListener("click", inpt7);
 document.getElementById("btn8").addEventListener("click", inpt8);
 document.getElementById("btn9").addEventListener("click", inpt9);
 document.getElementById("btn0").addEventListener("click", inpt0);
-document.getElementById("dot").addEventListener("click", inptdot);
-document.getElementById("plus").addEventListener("click", plus);
-document.getElementById("into").addEventListener("click", into);
-document.getElementById("minus").addEventListener("click", minus);
-
